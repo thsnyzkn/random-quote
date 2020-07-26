aaab367cff87ad8a4cb6686eafaedd21239b9fb1
Tahsin Yazkan <tahsinyazkan@gmail.com>
2020-07-26 15:09:50 +0300

    initialized branch, pretty much completed the project without proper styling& several caveats.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const quotesCount = quotes.length;
+  const [{ quote, author }, setContent] = useState({});
+  useEffect(() => {
+    const quotesList =
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+    const fetchData = async () => {
+      const response = await fetch(quotesList);
+      const json = await response.json();
+      setQuotes(json.quotes);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const randomValue = Math.floor(Math.random() * Math.floor(quotesCount));
+    setContent({
+      quote: quotes[randomValue]?.quote,
+      author: quotes[randomValue]?.author,
+    });
+  }, [quotes, quotesCount]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div
+        id="quote-box"
+        style={{
+          background: "#F9665E",
+          borderRadius: "24px",
+          minHeight: "450px",
+          margin: "5% 30%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <span id="text">{quote}</span>
+        <span id="author">{author}</span>
+        <button
+          style={{
+            backgroundColor: "#EEF1E6",
+            color: "#799FCB",
+            border: "none",
+            fontSize: "1rem",
+            outline: "none",
+            cursor: "pointer",
+            borderRadius: 12,
+            padding: "18px 36px",
+          }}
+          id="new-quote"
+          onClick={() => {
+            const randomValue = Math.floor(
+              Math.random() * Math.floor(quotesCount)
+            );
+            setContent({
+              quote: quotes[randomValue]?.quote,
+              author: quotes[randomValue]?.author,
+            });
+          }}
+        >
+          New Quote
+        </button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href={`https://twitter.com/intent/tweet?text=${quote}`}
           target="_blank"
           rel="noopener noreferrer"
+          id="tweet-quote"
         >
-          Learn React
+          Tweet
         </a>
-      </header>
+      </div>
     </div>
   );
 }
